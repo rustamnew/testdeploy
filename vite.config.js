9b99efb84ae59a5255a4677b8e3a41819f9179aa
rustamnew <1rustamnew1@gmail.com>
2023-10-19 22:50:46 +0300

    3

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: '/rustam_project/',
+    base: '/testdeploy/',
     plugins: [vue()],
     resolve: {
     alias: {
