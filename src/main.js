64b90d829c0d45cdf96ec9312aae2910fb6f8740
rustamnew <1rustamnew1@gmail.com>
2023-11-21 02:40:27 +0300

    1

--- src/main.js
+++
@@ -1,9 +0,0 @@
-import './assets/main.css'
-
-import { createPinia } from 'pinia'
-
-
-import { createApp } from 'vue'
-import App from './App.vue'
-
-createApp(App).use(createPinia()).mount('#app')
