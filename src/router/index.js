5b3c4570a63ee6e90a96804e011d976ff0d993e2
rustamnew <1rustamnew1@gmail.com>
2023-11-30 01:29:35 +0300

    1

--- src/router/index.js
+++ src/router/index.js
@@ -23,6 +23,7 @@ const routes = [
 const router = new VueRouter({
   routes,
   mode: 'history',
+  base: publicPath
 })
 
 
--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 
 module.exports = defineConfig({
-    publicPath: '/testdeploy/',
+    publicPath: process.env.NODE_ENV === 'production' ? '/testdeploy/' : '/',
     transpileDependencies: true,
     chainWebpack: config => {
         config.module.rules.delete("svg");
