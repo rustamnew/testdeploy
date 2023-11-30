5252c94b91ad2b541d821eb2a11222a0166aa2e8
rustamnew <1rustamnew1@gmail.com>
2023-11-30 04:29:04 +0300

    3123

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 
 module.exports = defineConfig({
+    publicPath: process.env.NODE_ENV === 'production' ? '/testdeploy/' : '/',
     transpileDependencies: true,
     chainWebpack: config => {
         config.module.rules.delete("svg");
