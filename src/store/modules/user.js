386a62712baaa0bce6d8201a5578b27655433a2f
rustamnew <1rustamnew1@gmail.com>
2023-11-30 04:32:10 +0300

    f

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -9,7 +9,7 @@ export default {
     },
     actions: {
         async registerUser(context, data) {
-            await axios.post('https://dist.nd.ru/api/reg', {
+            await axios.post('https://cors-anywhere.herokuapp.com/https://dist.nd.ru/api/reg', {
                 email: data.email,
                 password: data.password,
                 confirm_password: data.confirm_password,
@@ -27,12 +27,8 @@ export default {
         },
 
         async loginUser(context, data) {
-            /*await axios.post('https://dist.nd.ru/api/auth', {
-                email: data.email,
-                password: data.password
-            })*/
             await axios({
-                url: 'https://cors-anywhere.herokuapp.com/https://dist.nd.ru/api/auth',
+                url: 'https://cors-anywhere.herokuapp.com/https://cors-anywhere.herokuapp.com/https://dist.nd.ru/api/auth',
                 method: 'POST',
                 data: {
                     email: data.email,
@@ -67,7 +63,7 @@ export default {
         },
 
         async logoutUser(context) {
-            await axios.delete('https://dist.nd.ru/api/auth')
+            await axios.delete('https://cors-anywhere.herokuapp.com/https://dist.nd.ru/api/auth')
             .then((/*response*/) => {
                 context.commit('updateUser', {})
             })
