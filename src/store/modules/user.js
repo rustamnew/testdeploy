c5763ad07365e5638ca559e2d5b41d72c5bc6b50
rustamnew <1rustamnew1@gmail.com>
2023-11-30 03:56:29 +0300

    123

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -35,7 +35,7 @@ export default {
                 url: 'https://dist.nd.ru/api/auth',
                 method: 'POST',
                 mode: 'cors',
-                withCredentials: true,
+                withCredentials: false,
                 data: {
                     email: data.email,
                     password: data.password
