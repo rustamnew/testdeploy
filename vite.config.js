e5395acdc47ef8cb7dad1638ce7990e7f49a5854
rustamnew <1rustamnew1@gmail.com>
2024-02-19 00:51:25 +0300

    1

--- vite.config.js
+++ vite.config.js
@@ -5,13 +5,20 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: '/testdeploy/',
   plugins: [
     vue(),
   ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  server: {
+    proxy: {
+        '^/yindex.php': {
+            target: 'https://www.klerk.ru',
+            changeOrigin: true
+        }
     }
   }
 })
